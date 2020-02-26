5f3ac1fa16c2289bf7b0d92fb6e01a86dc84a168
Reny Ramos <renyramosgarcia@gmail.com>
2020-02-25 22:47:53 -0400

    Service provider con carga de rutas

---
+++ src/JsUtilitiesServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DesolatorMagno\JsUtilities;
+
+use Illuminate\Support\ServiceProvider;
+
+class JsUtilitiesServiceProvider extends ServiceProvider
+{
+    /**
+     * Publishes configuration file.
+     *
+     * @return  void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'js-utilities');
+
+    }
+
+    /**
+     * Make config publishment optional by merging the config from the package.
+     *
+     * @return  void
+     */
+    public function register()
+    {
+
+    }
+}
